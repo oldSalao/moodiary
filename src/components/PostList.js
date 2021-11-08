6ff5195740b698a2534a7b60bdb8a6e08df8e624
oldsalao <bucktoothvillain@gmail.com>
2021-11-09 01:06:15 +0900

    Feat : PostList 컴포넌트 스타일링

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,27 +1,86 @@
+import { Skeleton } from "@mui/material";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const PostListBox = styled.div`
   display: flex;
   flex-direction: column;
+  height: 50%;
+`;
+
+const PostListHeaderBox = styled.div``;
+
+const PostListBodyBox = styled.div``;
+
+const TitleBox = styled.div`
+  &:not(:last-child) {
+    border-bottom: 1px solid darkgrey;
+  }
+  padding-bottom: 1rem;
+  margin-bottom: 1rem;
+`;
+
+const PostListButtonBox = styled.button`
+  border: none;
+  border-radius: 4px;
+  font-size: 1rem;
+  font-weight: bold;
+  padding: 0.25rem 1rem;
+  color: black;
+  outline: none;
+  cursor: pointer;
 `;
 
 const PostList = ({ date, posts, loading }) => {
+  const moodImoji = {
+    happy: "😁",
+    delight: "😄",
+    excited: "😃",
+    soso: "🙂",
+    sad: "😥",
+    gloomy: "😭",
+    angry: "👿",
+  };
+
   if (loading) {
-    return <div>로딩중</div>;
+    return (
+      <PostListBox>
+        <PostListHeaderBox>
+          <h2>
+            <Skeleton variant="rectangular" width={130} height={32} />
+          </h2>
+          <hr style={{ width: "100%", marginBottom: "1rem" }} />
+        </PostListHeaderBox>
+        <PostListBodyBox>
+          <Skeleton variant="rectangular" width={130} height={150} />
+        </PostListBodyBox>
+      </PostListBox>
+    );
   }
 
   return (
     <PostListBox>
-      <h2>{date}</h2>
-      {posts &&
-        posts.map((post) => {
-          return (
-            <Link key={post.postId} to={`/post?postId=${post.postId}`}>
-              <div>{post.title}</div>
-            </Link>
-          );
-        })}
+      <PostListHeaderBox>
+        <h2>{date}</h2>
+        <hr style={{ width: "100%", marginBottom: "2rem" }} />
+      </PostListHeaderBox>
+      <PostListBodyBox>
+        {posts &&
+          posts.map((post) => {
+            return (
+              <TitleBox>
+                <Link
+                  key={post.postId}
+                  to={`/post?postId=${post.postId}`}
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  {moodImoji[post.mood.toLowerCase()]} {post.title}
+                </Link>
+              </TitleBox>
+            );
+          })}
+      </PostListBodyBox>
+      <PostListButtonBox>추가</PostListButtonBox>
     </PostListBox>
   );
 };
